c427e4d989a3b0381dfa9d60d9652e5bd271f5a3
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-05-24 10:15:10 +0530

    Update const.py

--- custom_components/rako/const.py
+++ custom_components/rako/const.py
@@ -1,19 +1,2 @@
 """Constants for the Rako integration."""
-
 DOMAIN = "rako"
-
-# Configuration keys
-CONF_HOST = "host"
-CONF_MAC = "mac"
-CONF_NAME = "name"
-CONF_PORT = "port"
-
-# Default values
-RAKO_BRIDGE_DEFAULT_PORT = 9761
-
-# Device types
-DEVICE_TYPE_LIGHT = "light"
-DEVICE_TYPE_SWITCH = "switch"
-
-# Logging
-LOG_LEVEL = "debug"
